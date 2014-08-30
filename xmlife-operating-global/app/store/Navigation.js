3f415dbb96affa430cad8ffd388e12e71d6492a4
yls <yls@94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c>
2014-08-30 10:03:45 +0000

    Description: 树结构 Reviewer： Bug ID: Tools check: Done
    git-svn-id: svn://192.168.6.101/paitao/xmlife/operating/trunk/code/trunk@22401 94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c

--- xmlife-operating-global/app/store/Navigation.js
+++ xmlife-operating-global/app/store/Navigation.js
@@ -9,83 +9,95 @@ Ext.define('XMLifeOperating.store.Navigation', {
     root: {
         text: 'Root',
         expanded: true,
-        children: [{
-            id: 'centralPointManage',
-            text: '中心管理',
-            leaf: false,
-            expanded: true,
-            children: [{
-                id: 'shop',
-                text: '店铺管理',
-                leaf: true
-            }, {
-                id: 'deliverAddress',
-                text: '配送地址管理',
-                leaf: true
-            }, {
-                id: 'lineList',
-                text: '线路管理',
-                leaf: true
-            }]
-        }, {
-            id: 'operateManage',
-            text: '操作管理',
-            left: false,
-            expanded: true,
-            children: [{
-                id: 'centerReceiveList',
-                text: '货到中心管理',
-                leaf: true
-            }, {
-                id: 'problemDealsList',
-                text: '问题订单管理',
-                leaf: true
-            }, {
-                id: 'realTimeList',
-                text: '数据中心',
-                leaf: true
-            }, {
-                id: 'cashOnDeliveryList',
-                text: '货到付款管理',
-                leaf: true
-            }, {
-                id: 'refundList',
-                text: '退款管理',
-                leaf: true
-            }, {
-                id: 'refundHistoryList',
-                text: '退款订单查询',
-                leaf: true
-            }]
-        }, {
-            id: 'stuffManage',
+        children: [
+        {
+            id: 'supportedCityList',
+            text: '城市开放管理',
+            leaf: true
+        }, 
+        {
+            id: 'staffManage',
             text: '员工管理',
             leaf: false,
+            expanded: true,
             children: [{
-                id: 'buyerList',
+                id: 'shopperList',
                 text: '买手管理',
                 leaf: true
             }, {
                 id: 'courierList',
                 text: '配送员管理',
                 leaf: true
+            }, {
+                id: 'otherList',
+                text: '其他员工管理',
+                leaf: true
             }]
-        }, {
+        }, 
+        {
+            id: 'templateManage',
+            text: '模板管理',
+            leaf: false,
+            expanded: true,
+            children: [{
+                id: 'productTemplateList',
+                text: '商品模板管理',
+                leaf: true
+            }, {
+                id: 'templateList',
+                text: '店铺模板管理',
+                leaf: true
+            }]
+        },
+        {
+            id: 'rechargeableCardManage',
+            text: '充值卡管理',
+            expanded: true,
+            leaf: false,
+            children: [{
+                id: 'rechargeableCardList',
+                text: '充值卡实例管理',
+                leaf: true
+            }, {
+                id: 'rechargeableCardTemplateList',
+                text: '充值卡模板管理',
+                leaf: true
+            }]
+        }, 
+
+        {
             id: 'customerManage',
             text: '用户管理',
             leaf: false,
+            expanded: true,
             children: [{
                 id: 'customerList',
                 text: '用户信息管理',
                 leaf: true
             }, {
-                id: 'feedbackResource',
+                id: 'feedbackList',
                 text: '用户反馈管理',
                 leaf: true
             }]
-        }, {
-            id: 'orderList',
+        }, 
+        {
+            id: 'dealList',
             text: '订单管理',
+            leaf: true
+        }, 
+        {
+            id: 'refundList',
+            text: '退款管理',
+            leaf: true
+        },
+        {
+            id: 'authorityManage',
+            text: '权限管理',
+            leaf: true
+        },
+        {
+            id: 'shoparea',
+            text: '中心平台',
             leaf: true
         }]
     }
