9529e0897183e5078f4dcd3e8134b40a81b39dbe
ljlong <ljlong@94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c>
2014-09-01 13:28:03 +0000

    店铺管理bug修复
    git-svn-id: svn://192.168.6.101/paitao/xmlife/operating/trunk/code/trunk@22554 94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c

---
+++ xmlife-operating-shoparea/app/view/centralPointManage/shop/ShopTab.js
@@ -0,0 +1,57 @@
+/**
+ * @class SimpleTasks.view.lists.Tree
+ * @extends Ext.tree.Panel
+ * The task list view.  A tree that displays all of the task lists.
+ */
+
+ Ext.define('XMLifeOperating.view.centralPointManage.shop.ShopTab', {
+    extend: 'Ext.tab.Panel',
+    xtype: 'shoptab',
+    header: false,
+    itemId:'shopInfoTab',
+    items:[
+            {
+                title: '店铺信息',
+                id:'tab1',
+                autoScroll: true,
+                layout:'fit',
+                items:{
+                    xtype:'shopInfo'
+                }
+            },
+            {
+                title: '货架管理',
+                id:'tab2',
+                layout:'fit',
+                items:{
+                    xtype:'productCollection'
+                }
+            },
+            {
+                title:'商品管理',
+                id:'tab3',
+                layout:'fit',
+                items:{
+                    xtype:'productList'
+                }
+            },
+            {
+                title:'快递员管理',
+                id:'tab4',
+                layout:'fit',
+                items:{
+                    xtype:'shopDelivererList'
+                }
+            },
+            {
+                title:'账单管理',
+                layout:'fit',
+                items:{
+                    xtype:'shopOrder'
+                }
+            }
+        ]
+    
+
+
+});
