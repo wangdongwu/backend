f5cc25a3e36b8249326cde4a95c4e1c9599ccc20
wdw <wdw@94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c>
2014-09-11 13:07:17 +0000

    修改买手管理 list 数据参数
    git-svn-id: svn://192.168.6.101/paitao/xmlife/operating/trunk/code/trunk@23335 94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c

--- xmlife-operating-global/app/store/Shopper.js
+++ xmlife-operating-global/app/store/Shopper.js
@@ -2,5 +2,5 @@ Ext.define('XMLifeOperating.store.Shopper', {
     extend: 'Ext.data.Store',
     model:'XMLifeOperating.model.Shopper',
     autoLoad: {start: 0, limit: 25,page:1},
-    proxy : new XMLifeOperating.generic.BaseProxy('shopper','arrayResult')
+    proxy : new XMLifeOperating.generic.BaseProxy('shopper','result')
 });
