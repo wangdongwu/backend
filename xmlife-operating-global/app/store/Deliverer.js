b40208298396cc5fdabc3827052ee2825b27ba10
wdw <wdw@94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c>
2014-09-12 02:40:09 +0000

    修改买手管理 list 数据参数
    git-svn-id: svn://192.168.6.101/paitao/xmlife/operating/trunk/code/trunk@23400 94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c

--- xmlife-operating-global/app/store/Deliverer.js
+++ xmlife-operating-global/app/store/Deliverer.js
@@ -2,5 +2,5 @@ Ext.define('XMLifeOperating.store.Deliverer', {
     extend: 'Ext.data.Store',
     model:'XMLifeOperating.model.Deliverer',
     autoLoad: {start: 0, limit: 25,page:1},
-    proxy : new XMLifeOperating.generic.BaseProxy('deliverer','arrayResult')
+    proxy : new XMLifeOperating.generic.BaseProxy('deliverer','result')
 });
