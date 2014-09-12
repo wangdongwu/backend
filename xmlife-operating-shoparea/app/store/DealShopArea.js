7938dbc16a4901d275f19227c53ad63fb100fb00
yls <yls@94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c>
2014-09-12 09:51:57 +0000

    Description:货到中心 分页 Reviewer： Bug ID: Tools check: Done
    git-svn-id: svn://192.168.6.101/paitao/xmlife/operating/trunk/code/trunk@23541 94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c

--- xmlife-operating-shoparea/app/store/DealShopArea.js
+++ xmlife-operating-shoparea/app/store/DealShopArea.js
@@ -1,5 +1,5 @@
 Ext.define('XMLifeOperating.store.DealShopArea', {
     extend: 'Ext.data.Store',
     model:'XMLifeOperating.model.DealShopArea',
-    proxy:new XMLifeOperating.generic.BaseProxy('deal/shopArea', 'arrayResult')
+    proxy:new XMLifeOperating.generic.BaseProxy('deal/shopArea', 'result')
 });
