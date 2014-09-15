6fe798a720d577348d573cb8b098030439d6ec8e
ryl <ryl@94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c>
2014-09-15 09:36:04 +0000

    git-svn-id: svn://192.168.6.101/paitao/xmlife/operating/trunk/code/trunk@23937 94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c

--- xmlife-operating-shoparea/app/view/dealManage/DealList.js
+++ xmlife-operating-shoparea/app/view/dealManage/DealList.js
@@ -28,11 +28,11 @@ Ext.define('XMLifeOperating.view.dealManage.DealList', {
             xtype: 'button',
             itemId: 'paymentInvoice',
             text: '导出昨日支付对账单',
-        },{
+        },/*{
             xtype: 'button',
             itemId: 'checkUnallocatedOrder',
             text: '查看未分配订单',
-        },
+        },*/
         '->', {
             xtype: 'textfield',
             emptyText: '输入搜索号码...',
