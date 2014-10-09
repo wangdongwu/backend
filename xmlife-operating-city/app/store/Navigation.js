6120b16422ad712348989d287b3ea684ba1114ff
ljlong <ljlong@94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c>
2014-10-09 07:16:06 +0000

    git-svn-id: svn://192.168.6.101/paitao/xmlife/operating/trunk/code/trunk@25837 94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c

--- xmlife-operating-city/app/store/Navigation.js
+++ xmlife-operating-city/app/store/Navigation.js
@@ -1,7 +1,7 @@
 Ext.define('XMLifeOperating.store.Navigation', {
     requires: [
-        'Ext.grid.*',
-        'Ext.data.TreeStore',
+    'Ext.grid.*',
+    'Ext.data.TreeStore',
     ],
     extend: 'Ext.data.TreeStore',
     fields: ['id', 'text', 'code', 'leaf'],
@@ -10,23 +10,27 @@ Ext.define('XMLifeOperating.store.Navigation', {
         text: 'Root',
         expanded: true,
         children: [{
-                id: 'freightList',
-                text: '运费管理',
-                leaf: false
-            }, {
-                id: 'shopareaList',
-                text: '中心平台管理',
-                leaf: true
-            }, {
-                id: 'AuthorityTabPanel',
-                text: '权限管理',
-                leaf: false
-            }
+            id: 'freightList',
+            text: '运费管理',
+            leaf: true
+        }, {
+            id: 'shopareaList',
+            text: '中心平台管理',
+            leaf: true
+        }, {
+            id: 'AuthorityTabPanel',
+            text: '中心权限管理',
+            leaf: true
+        },{
+            id: 'personalCenter',
+            text: '个人中心',
+            leaf: true
+        }
             /*, {
                 id: 'BatchAddEstate',
                 text: '批量添加小区',
                 leaf: true
             }*/
-        ]
-    }
-});
+            ]
+        }
+    });
