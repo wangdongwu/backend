b6ff03939dcd5ab5b18bb94fdc4bfcb4ec0a8b9a
ryl <ryl@94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c>
2014-10-11 05:40:05 +0000

    git-svn-id: svn://192.168.6.101/paitao/xmlife/operating/trunk/code/trunk@25949 94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c

--- xmlife-operating-global/app/view/staffManage/shopper/GShopperEdit.js
+++ xmlife-operating-global/app/view/staffManage/shopper/GShopperEdit.js
@@ -64,31 +64,6 @@ Ext.define('XMLifeOperating.view.staffManage.shopper.GShopperEdit', {
                     valueField: 'value',
                     allowBlank:false                  
                 },
-                /*{
-                    fieldLabel : '市场类型',
-                    labelWidth: 90,
-                    store : typeStore,
-                    name : 'type',
-                    allowBlank : false,
-                    xtype : 'combo',
-                    editable : false,
-                    queryMode : 'local',
-                    triggerAction : 'all',
-                    displayField: 'type',
-                    valueField: 'value',
-                    allowBlank:false                  
-                },*/
-                /*{
-                    xtype: 'textfield',
-                    name: 'avatar',
-                    fieldLabel: '图片',
-                    labelWidth: 90,
-                    allowBlank:false,
-                    minLength: 2,
-                    minLengthText:'图片最小长度为2',
-                    maxLength: 10,
-                    maxLengthText:'图片最大长度为10',
-                },*/
                 {
                     xtype: 'fieldset',
                     layout: 'column',
