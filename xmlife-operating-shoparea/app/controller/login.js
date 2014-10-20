0779409e6e078a293ce3943847ba8bd6ca20c74a
wdw <wdw@94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c>
2014-10-20 09:14:56 +0000

    中心管理 login 根据账号 加载相应的tree
    git-svn-id: svn://192.168.6.101/paitao/xmlife/operating/trunk/code/trunk@26614 94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c

--- xmlife-operating-shoparea/app/controller/login.js
+++ xmlife-operating-shoparea/app/controller/login.js
@@ -1,195 +1,145 @@
 Ext.define('XMLifeOperating.controller.login', {
-    extend: 'Ext.app.Controller',
-    views : ['admin.login','Toolbar','Navigation'],
-    stores : ['Navigation'],
-    refs : [
-        {
-             ref: 'login',
-             selector: 'login',
-             xtype: 'login',
-             autoCreate: true
-        },
-        {
-          ref : 'headerToolbar',
-          selector : 'headerToolbar',
-          xtype: 'headerToolbar',
-          autoCreate: true
-        },
-        {
-          ref: 'currentUsername',
-          selector: '#txtUserName'
-      },
-      {
-        ref : 'cmbGlobalCity',
-        selector : '#cmbGlobalCity'
-      }
-    ],
-    init : function(){
-      var self = this;
-      var sessionId = localStorage.getItem('sessionId'),
-          username = localStorage.getItem('username');
+  extend: 'Ext.app.Controller',
+  views: ['admin.login', 'Toolbar', 'Navigation'],
+  stores: ['ShopArea', 'Navigation'],
+  models: ['ShopArea'],
+  refs: [{
+    ref: 'login',
+    selector: 'login',
+    xtype: 'login',
+    autoCreate: true
+  }, {
+    ref: 'headerToolbar',
+    selector: 'headerToolbar',
+    xtype: 'headerToolbar',
+    autoCreate: true
+  }, {
+    ref: 'currentUsername',
+    selector: '#txtUserName'
+  }, {
+    ref: 'cmbGlobalCity',
+    selector: '#cmbGlobalCity'
+  }, {
+    ref: 'cmbGlobalCenter',
+    selector: 'headerToolbar #cmbGlobalCenter',
+  }, {
+    ref: 'txtShopAreaName',
+    selector: 'headerToolbar #txtShopAreaName'
+  }],
+  init: function() {
+    var self = this;
+    var sessionId = localStorage.getItem('sessionId');
+    var areaId = localStorage.getItem('areaId');
 
-      if(!sessionId){
-        this.getLogin().show();
-        }else{
-        //self.getCurrentUsername().setText(username);
-        Ext.Ajax.defaultHeaders = {'auth-token' : sessionId};
-        self.getNavigationStore().setRootNode({expanded:true});
-      };
-
-        this.control({
-          '#login-bt' : {
-            click : self.login
-          },
-          'headerToolbar #txtUserName' : {
-          click : function(){
-            var sessionId = localStorage.getItem('sessionId');
-            if(!sessionId){
-              this.getLogin().show();
-            }
-          }
-        },
-        'headerToolbar #editAdmin' : {
-          click : function(){
-            var loginView = this.getLogin(),
-                form = loginView.down('form'),
-                formEl = form.getForm(),
-                username = localStorage.getItem('username'),
-                nickField = loginView.down('[name=nick]'),
-                usernameField = loginView.down('[name=username]'),
-                passwordField = loginView.down('[name=password]'),
-                newPasswordField = loginView.down('[name=newPassword]'),
-                reNewPasswordField = loginView.down('[name=reNewPassword]'),
-                loginBt = loginView.down('#login-bt');
-
-                formEl.reset();
-                Ext.Ajax.request({
-                  url : XMLifeOperating.generic.Global.URL.biz +'admin/getInfo',
-                  success : function(result){
-                    var data = Ext.decode(result.responseText);
-                    nickField.show().setValue(data.name).setDisabled(true);
-                    usernameField.setValue(data.account).setDisabled(true);
-                  }
-                })
-
-                newPasswordField.show();
-                reNewPasswordField.show()
-
-                passwordField.setFieldLabel('旧密码');
-                loginBt.setText('确定');
-                self.edit = true;
-                loginView.show();
-
-          }
-        },
-        'headerToolbar #btnSignOut' : {
-          click : function(){
-              var loginOutUrl = XMLifeOperating.generic.Global.URL.biz + 'admin/logout',
-                  sessionId = localStorage.getItem('sessionId');
-                  Ext.Ajax.request({
-                      url: loginOutUrl,
-                      method: 'post',
-                      success : function(response){
-                        if(response.responseText){
-                          localStorage.removeItem('sessionId');
-                          localStorage.removeItem('username');
-                          window.location.reload();
-                        }
-                      },
-                      failure : function(response){
-                          Ext.MessageBox.show({
-                            title: '注销失败',
-                            msg: '您现在已经注销了!',
-                            buttons: Ext.Msg.OK
-                         });
-                        
-                      }
-                    })
-                              
-          }
-        }
-        }
-        )
-      
-    },
-    login : function(){
-      var self = this,
-          view = this.getLogin(),
-          username = view.down('[name=username]').getValue(),
-          password = view.down('[name=password]').getValue(),
-          newPassword = view.down('[name=newPassword]').getValue();
-          reNewPassword = view.down('[name=reNewPassword]').getValue();
-          loginUrl = XMLifeOperating.generic.Global.URL.biz + 'admin/login',
-          updateUrl = XMLifeOperating.generic.Global.URL.biz + 'admin/update/ownAccount';
-          if(self.edit){
-            var newPassword = view.down('[name=newPassword]').getValue(),
-                name = view.down('[name=nick]').getValue();
-            Ext.Ajax.request({
-              url : updateUrl,
-              params : {account:username , name : name, oldPwd : password,newPwd : newPassword},
-              method : 'put',
-              success : function(response){
-                if(response.responseText == 1){
-                  self.getLogin().hide();
-                  Ext.Msg.alert('成功', '成功更新'+username+'账户');
-
-                }else{
-                  Ext.Msg.alert('失败', '更新'+username+'账户时失败');
-                } 
-              }
-            })
-          }else{
-            Ext.Ajax.request({
-            url: loginUrl,
-            params: {user:username,pwd:password},
-            method: 'post',
-            success : function(response){
-              if(response.responseText){
-                var data =  Ext.JSON.decode(response.responseText);
-                /*本地储存信息*/
-                localStorage.setItem("sessionId", data.sessionId);
-                localStorage.setItem("username", username);
-                /*更改页头*/
-                Ext.Ajax.defaultHeaders = {
-                    'auth-token' : data.sessionId
-                };
-                /*设置用户名字*/
-                self.getCurrentUsername().setText(username);
-                /*加载tree*/
-                self.getNavigationStore().setRootNode({expanded:true});
-
-                view.hide();
-              }
-            },
-            failure : function(response){
-              if(response.status==401){
-                var data = {
-                  'Unauthorized' : '对不起，你登录失败了，请重新登录'
-                };
-                Ext.MessageBox.show({
-                  title: '登录失败',
-                  msg: data[response.statusText],
-                  buttons: Ext.Msg.OK
-               });
-              }
-            }
-          });  
-          }
-          
-    },
-    checkPassword : function(str){
-      var resArr = [/[a-z]/,/[A-Z]/,/[0-9]/,/[@#$%]/],
-          num = 0;
-      Ext.each(resArr, function(res) {
-        if(res.test(str)){
-          num++
-        };
-      });
-      if (num >=3) {
-        return true;
-      }else{
-        return false;
-      };
+    if (areaId) {
+      XMLifeOperating.generic.Global.current_operating = areaId;
     }
-});
 
+    if (!sessionId) {
+      this.getLogin().show();
+      this.control({
+        '#login-bt': {
+          click: self.login
+        }
+      });
+
+    } else { //有session
+      Ext.Ajax.defaultHeaders = {
+        'auth-token': sessionId
+      };
+      self.detectAccount();
+      this.getShopAreaStore().load();
+      //Ext.getCmp('cmbGlobalCenter').setValue(areaId);
+      /*      self.getNavigationStore().setRootNode({
+        expanded: true
+      });*/
+    }
+  },
+  login: function() {
+    var self = this,
+      view = this.getLogin(),
+      username = view.down('[name=username]').getValue(),
+      password = view.down('[name=password]').getValue(),
+      loginUrl = XMLifeOperating.generic.Global.URL.biz + 'admin/login';
+    Ext.Ajax.request({
+      url: loginUrl,
+      params: {
+        user: username,
+        pwd: password
+      },
+      method: 'post',
+      success: function(response) {
+        if (response.responseText) {
+          var data = Ext.JSON.decode(response.responseText);
+          localStorage.setItem("sessionId", data.sessionId);
+          localStorage.setItem("username", username);
+          if (data.areaId) {
+            localStorage.setItem("areaId", data.areaId);
+            XMLifeOperating.generic.Global.current_operating = data.areaId
+          }
+          if (data.areaName) {
+            localStorage.setItem("areaName", data.areaName);
+          }
+
+          Ext.Ajax.defaultHeaders = {
+            'auth-token': data.sessionId
+          };
+          self.getCurrentUsername().setText(username);
+          /*加载tree*/
+          /*self.getNavigationStore().setRootNode({
+            expanded: true
+          });*/
+          self.detectAccount();
+          view.hide();
+          self.getShopAreaStore().load();
+          //Ext.getCmp('cmbGlobalCenter').setValue(data.areaId);
+          self.getTxtShopAreaName().setText(data.areaName);
+        }
+      },
+      failure: function(response) {
+        if (response.status == 401) {
+          var data = {
+            'Unauthorized': '对不起，你登录失败了，请重新登录'
+          }
+          Ext.MessageBox.show({
+            title: '登录失败',
+            msg: data[response.statusText],
+            buttons: Ext.Msg.OK
+          });
+        }
+      }
+    });
+  },
+  detectAccount: function() {
+    var me = this;
+    var store = me.getNavigationStore();
+    var success = function(response) {
+      var obj = eval('(' + response.responseText + ')');
+      var type = obj.adminType;
+      if (type == 'Area') { //中心长账号登陆
+        store.setProxy(new XMLifeOperating.generic.BaseProxy('module/getUserModulesTree'));
+        XMLifeOperating.generic.Global.current_operating = obj.areaId;
+        store.setRootNode({
+          expanded: true
+        });
+      } else if (type == 'City' || type == 'Global') { //高级权限账号登陆
+        store.setProxy(new XMLifeOperating.generic.BaseProxy('module/getPlatModulesTree'));
+        me.getCmbGlobalCenter().show();
+        store.getProxy().extraParams = {
+          type: 'Area'
+        };
+        store.setRootNode({
+          expanded: true
+        });
+      }
+    }
+    var failure = function(response) {
+      Ext.MessageBox.show({
+        title: '账号检测失败',
+        msg: data[responseText.statusText],
+        buttons: Ext.Msg.OK
+      });
+    }
+    sendGetRequest('admin/getInfo', null, '检测账号', '检测账号成功', '检测账号失败', success, failure);
+  }
+});
