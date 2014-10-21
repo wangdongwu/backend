e04e876e7043c005f3637eeb55a294f6a2635ed7
ryl <ryl@94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c>
2014-10-21 08:24:43 +0000

    git-svn-id: svn://192.168.6.101/paitao/xmlife/operating/trunk/code/trunk@26749 94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c

--- xmlife-operating-shoparea/resources/url_config.js
+++ xmlife-operating-shoparea/resources/url_config.js
@@ -3,12 +3,12 @@ window.URLMap = {
 	//biz     : 'http://192.168.6.187:8080/',
 	//biz     : 'http://192.168.5.131:8080/',
 	//biz :'http://192.168.5.86:8089/',
-	//biz:'http://192.168.5.190:8089/',//mzg
+	//biz:'http://192.168.5.190:1550/',//mzg
 
 	//biz: 'http://192.168.7.119:8089/',//sc
-	biz: 'http://192.168.5.86:1550/', //cjh
-	//biz: 'http://dev.xiaomei.com:1550/', //dev
-	//biz: 'http://192.168.7.155:8089/',//lc note
+	//biz: 'http://192.168.5.86:1550/', //cjh
+	biz: 'http://dev.xiaomei.com:1550/', //dev
+	//biz: 'http://192.168.7.155:1550/',//lc note
 	res: 'http://dev.xiaomei.com/res/image/id-',
 	upload: 'http://dev.xiaomei.com/res/upload'
 };
