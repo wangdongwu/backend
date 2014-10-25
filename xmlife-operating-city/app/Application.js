4adf6857886a472dbd76d63365cf860d229d875c
wdw <wdw@94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c>
2014-10-25 07:18:50 +0000

    添加splashscreen
    git-svn-id: svn://192.168.6.101/paitao/xmlife/operating/trunk/code/trunk@27064 94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c

--- xmlife-operating-city/app/Application.js
+++ xmlife-operating-city/app/Application.js
@@ -46,6 +46,7 @@ Ext.define('XMLifeOperating.Application', {
             };
         }
 
+      var splashscreen = Ext.getBody().mask('<div style="text-align:center;width:300px;">正在加载小美后台应用...<br/>请稍等</div>', 'splashscreen');
 
         var task = new Ext.util.DelayedTask(function() {
             // Fade out the body mask
