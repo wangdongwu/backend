73b26bc8661ec6e0c6445657c5fe653da3c7c8de
wwb <wwb@94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c>
2014-10-27 10:17:40 +0000

    系统消息和短信
    git-svn-id: svn://192.168.6.101/paitao/xmlife/operating/trunk/code/trunk@27134 94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c

---
+++ xmlife-operating-global/app/store/Message.js
@@ -0,0 +1,17 @@
+Ext.define('XMLifeOperating.store.Message', {
+    extend: 'Ext.data.Store',
+    model:'XMLifeOperating.model.Message',
+    autoLoad: false,
+    proxy: new XMLifeOperating.generic.BaseProxy('notify/list')
+    // data:[
+    //     { 'id': '10001','content':'今天要发布系统','startTime':1414118179178,'adminName':'wwb','url':'http://www.xiaomei.com/send','file':'杭州用户UID.text','status': 0},
+    //     { 'id': '10002','content':'魂牵梦萦','startTime':1414118179178,'adminName':'hoby','url':'http://www.xiaomei.com/send','file':'杭州用户UID.text','status': 0},
+    //     { 'id': '10000','content':'I\'m love body good','startTime':1414118179178,'adminName':'hoby','url':'http://www.xiaomei.com/send','file':'杭州用户UID.text','status': 1}
+    // ],
+    // proxy: {
+    //     type: 'memory',
+    //     reader: {
+    //         type: 'json'
+    //     }
+    // }
+});
