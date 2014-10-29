19d02f52b810fa6f7f69485c09d4bb44a23d4a80
wdw <wdw@94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c>
2014-10-29 02:25:54 +0000

    修改URL
    git-svn-id: svn://192.168.6.101/paitao/xmlife/operating/trunk/code/trunk@27220 94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c

--- xmlife-operating-city/app/Application.js
+++ xmlife-operating-city/app/Application.js
@@ -125,7 +125,7 @@ var uploadImage = function(form, textfield) {
     var sessionId = localStorage.getItem('sessionId') || '';
 
     form.submit({
-        url: XMLifeOperating.generic.Global.URL.upload + '/?sessionId=' + sessionId,
+        url: XMLifeOperating.generic.Global.URL.upload + '?sessionId=' + sessionId,
         params: {
             'sessionId': sessionId
         },
