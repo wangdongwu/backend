a9bd4ad64c4a550a3bdeac390eff0dd1ee37f71d
wwb <wwb@94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c>
2014-11-04 06:57:35 +0000

    新版首页配置
    git-svn-id: svn://192.168.6.101/paitao/xmlife/operating/trunk/code/trunk@27918 94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c

---
+++ xmlife-operating-shoparea/app/model/HomePage.js
@@ -0,0 +1,4 @@
+Ext.define('XMLifeOperating.model.HomePage', {
+    extend: 'Ext.data.Model',
+    fields: ['id', 'areaId', 'layoutId', 'order', 'version', 'name', 'enable', 'image', 'title', 'url']
+});
