fc6fcdea0f74815f32187ec1a2eaa9858bf776c8
wwb <wwb@94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c>
2014-11-05 02:09:27 +0000

    新版首页配置
    git-svn-id: svn://192.168.6.101/paitao/xmlife/operating/trunk/code/trunk@27980 94407b05-e7ba-4d3b-a6a0-d8c7e7bf0b8c

--- xmlife-operating-shoparea/app/store/HomePageModuleDetail.js
+++ xmlife-operating-shoparea/app/store/HomePageModuleDetail.js
@@ -2,15 +2,4 @@ Ext.define('XMLifeOperating.store.HomePageModuleDetail', {
 	extend: 'Ext.data.Store',
 	model: 'XMLifeOperating.model.HomePage',
 	proxy: new XMLifeOperating.generic.BaseProxy('homepage/getModuleItems')
-	// data: [
-	// 	{'id':'10000', 'order':1, 'image':'541432dc03643ef8f885fc61db', 'title':'小美生活', 'url':'http://www.xiaomei.com'},
-	// 	{'id':'10001', 'order':2, 'image':'541432dc03643ef8f885fc61db', 'title':'小美生活', 'url':'http://www.xiaomei.com'},
-	// 	{'id':'10002', 'order':3, 'image':'541432dc03643ef8f885fc61db', 'title':'小美生活', 'url':'http://www.xiaomei.com'}
-	// ],
-	// proxy: {
-	// 	type: 'memory',
-	// 	reader: {
-	// 		type: 'json'
-	// 	}
-	// }
 });
