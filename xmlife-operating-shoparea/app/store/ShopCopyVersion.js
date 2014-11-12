2dc0b9ed3921b85192b559f741e75e1a896f3d9e
wangdongwu <wdw@ipaitao.com>
2014-11-12 09:14:46 +0800

    shopconfig 店铺选择

---
+++ xmlife-operating-shoparea/app/store/ShopCopyVersion.js
@@ -0,0 +1,21 @@
+/**
+ * @author wangdongwu
+ */
+Ext.define('XMLifeOperating.store.ShopCopyVersion', {
+    extend: 'Ext.data.Store',
+    model: 'XMLifeOperating.model.ShopConfig',
+    proxy : new XMLifeOperating.generic.BaseProxy('shopHomepage')
+    /*data : [{
+      id : 1,
+      name : '测试111111111',
+      enable : 0
+    },{
+      id : 2,
+      name : '测试222222222',
+      enable : 1
+    },{
+      id : 3,
+      name : '测试333333333',
+      enable : 1
+    }]*/
+});
