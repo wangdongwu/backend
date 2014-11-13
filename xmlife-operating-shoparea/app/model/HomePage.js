86895ded6cc846ce90a76041652a1ddc939e50b8
wanbao.wang <w.hoby@qq.com>
2014-11-13 09:44:29 +0800

    combo取值bug修复

--- xmlife-operating-shoparea/app/model/HomePage.js
+++ xmlife-operating-shoparea/app/model/HomePage.js
@@ -1,11 +1,17 @@
 Ext.define('XMLifeOperating.model.HomePage', {
     extend: 'Ext.data.Model',
     fields: [
-    	'areaId', 'status','id', 'layoutId', 'order', 'index', 'version', 'enable',
-    	'type', 'items', 'name', 'image', 'urlType', 'url', 'shopId', 'cid', 'pid',
+    	'areaId', 'status', 'id', 'layoutId', 'order', 'index', 'version', 'enable',
+    	'type', 'items', 'image', 'urlType', 'url', 'shopId', 'cid', 'pid',
     	{name: 'titles', convert: function(v,r) {
-    			if(v) return v.join(';');
+    			if(typeof(v) == 'object' && v.length >0){
+    				return v.join(';');
+    			} else {
+    				return v;
+    			}
     		}
-    	}
+    	},
+        // fixed combo 有回车符时取值的bug
+        {name: 'name', convert: function(v,r) {return v.replace(/\n/g,' ');} }
     ]
 });
--- xmlife-operating-shoparea/app/store/HomePageCategory.js
+++ xmlife-operating-shoparea/app/store/HomePageCategory.js
@@ -1,5 +1,5 @@
 Ext.define('XMLifeOperating.store.HomePageCategory', {
 	extend: 'Ext.data.Store',
 	model: 'XMLifeOperating.model.HomePage',
-	proxy: new XMLifeOperating.generic.BaseProxy('category/roots')
+	proxy: new XMLifeOperating.generic.BaseProxy('category/getRootOnlineCategorys')
 });
