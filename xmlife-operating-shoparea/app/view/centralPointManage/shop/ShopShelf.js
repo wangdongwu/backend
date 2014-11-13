f90c4fa9250481e5d2b3e3b36bf22a04f2bb664a
ljlong <ljlong@ipaitao.com>
2014-11-13 10:32:36 +0800

    'merge2014/11/13'

--- xmlife-operating-shoparea/app/view/centralPointManage/shop/ShopShelf.js
+++ xmlife-operating-shoparea/app/view/centralPointManage/shop/ShopShelf.js
@@ -145,78 +145,77 @@ Ext.define('XMLifeOperating.view.centralPointManage.shop.ShopShelf', {
                 }
             }
             return returnStr;
-        },
-        tdCls: 'user-td'
+        }
     }],
-    dockedItems: [{
-        xtype: 'toolbar',
-        dock: 'top',
-        style: {
-            border: 'none'
-        },
-        items: [{
-            text: '添加货架',
-            itemId: 'openCreateShelvesWin'
-        }, {
-            xtype: 'button',
-            text: '状态查看',
-            menu: {
-                xtype: 'menu',
-                items: [{
-                    text: '所有下架商品',
-                    itemId: 'viewAllSoldOutProduct'
-                }, {
-                    text: '所有上架商品',
-                    itemId: 'viewAllOnlineProduct'
-                }, {
-
-                    text: '所有雪藏商品',
-                    itemId: 'viewAllHiddenProduct'
-                }, {
-                    text: '所有废弃商品',
-                    itemId: 'viewAllAbandonedProduct'
-                }]
-            }
-        }, {
-            xtype: 'button',
-            text: '保存排序',
-            itemId: 'saveShelvesOrder'
-        }, {
-            xtype: 'button',
-            text: '返回',
-            itemId: 'returnShopStore'
-        }, '->', {
-            xtype: 'textfield',
-            emptyText: '输入商品名称',
-            name: 'keyword',
-            itemId: 'keyword'
-
-        }, {
-            xtype: 'button',
-            itemId: 'productSearch',
-            text: '搜索',
-        }]
+dockedItems: [{
+    xtype: 'toolbar',
+    dock: 'top',
+    style: {
+        border: 'none'
+    },
+    items: [{
+        text: '添加货架',
+        itemId: 'openCreateShelvesWin'
     }, {
-        xtype: 'toolbar',
-        dock: 'bottom',
-        items: [{
-            xtype: 'pagingtoolbar',
-            itemId: 'pagetool',
-            store: 'CategoryRoots',
-            displayInfo: true,
-            style: 'border:none'
-        }]
-    }],
-    viewConfig: {
-        plugins: {
-            ptype: 'gridviewdragdrop',
-            dragText: 'Drag and drop to reorder'
-        },
-        listeners: {
-            drop: function(node, data, dropRec, dropPosition) {
-                var me = this;
-                //Ext.getCmp('rowIndex').update(this);
-            }
+        xtype: 'button',
+        text: '状态查看',
+        menu: {
+            xtype: 'menu',
+            items: [{
+                text: '所有下架商品',
+                itemId: 'viewAllSoldOutProduct'
+            }, {
+                text: '所有上架商品',
+                itemId: 'viewAllOnlineProduct'
+            }, {
+
+                text: '所有雪藏商品',
+                itemId: 'viewAllHiddenProduct'
+            }, {
+                text: '所有废弃商品',
+                itemId: 'viewAllAbandonedProduct'
+            }]
+        }
+    }, {
+        xtype: 'button',
+        text: '保存排序',
+        itemId: 'saveShelvesOrder'
+    }, {
+        xtype: 'button',
+        text: '返回',
+        itemId: 'returnShopStore'
+    }, '->', {
+        xtype: 'textfield',
+        emptyText: '输入商品名称',
+        name: 'keyword',
+        itemId: 'keyword'
+
+    }, {
+        xtype: 'button',
+        itemId: 'productSearch',
+        text: '搜索',
+    }]
+}, {
+    xtype: 'toolbar',
+    dock: 'bottom',
+    items: [{
+        xtype: 'pagingtoolbar',
+        itemId: 'pagetool',
+        store: 'CategoryRoots',
+        displayInfo: true,
+        style: 'border:none'
+    }]
+}],
+viewConfig: {
+    plugins: {
+        ptype: 'gridviewdragdrop',
+        dragText: 'Drag and drop to reorder'
+    },
+    listeners: {
+        drop: function(node, data, dropRec, dropPosition) {
+            var me = this;
+            //Ext.getCmp('rowIndex').update(this);
         }
     }
+}
 });
