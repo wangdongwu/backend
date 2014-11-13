7d792578b228d864a55d42dc19827713b8dbec33
wangdongwu <wdw@ipaitao.com>
2014-11-13 10:37:13 +0800

    shopconfig

---
+++ xmlife-operating-shoparea/app/model/CategoryLeaf.js
@@ -0,0 +1,44 @@
+Ext.define('XMLifeOperating.model.CategoryLeaf', {
+  extend: 'Ext.data.Model',
+  idProperty: 'id',
+  fields: [
+  'barCode',
+  'categoryId',
+  'categoryIds',
+  'categoryNames',
+  'categoryname',
+  'dprice',
+  'dprice2',
+  'fprice',
+  'fprice2',
+  'id',
+  'limitCount',
+  'limitType',
+  'pprice',
+  'pprice2',
+  'productLimitCount',
+  'rank',
+  'rank2',
+  'shopId',
+  'shopname',
+  'skuId',
+  'soldCount',
+  'status',
+  'statusCn',
+  'stock',
+  'templateId',
+  'top',
+  'transferDprice',
+  'transferFprice',
+  'transferPprice',
+  'unit',
+  'unitname',
+  {
+    name : 'name',
+    convert : function(v){
+      return v.replace(/\n/g,'');
+    }
+  }
+  ]
+});
+
