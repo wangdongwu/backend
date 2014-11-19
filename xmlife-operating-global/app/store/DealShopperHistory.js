c17e29576579f34f4e02c9b313ca64274524d5c3
yelinsen <yls@ipaitao.com>
2014-11-19 19:29:56 +0800

    Description：【全局后台】【员工管理】查看员工历史订单时列表为空，但是同一个员工中心后台可以查询到订单 Reviewer: Bug ID: MJS-1475 Tools check: Done Release：1.3

--- xmlife-operating-global/app/store/DealShopperHistory.js
+++ xmlife-operating-global/app/store/DealShopperHistory.js
@@ -2,7 +2,7 @@ Ext.define('XMLifeOperating.store.DealShopperHistory', {
     extend: 'Ext.data.Store',
     model:'XMLifeOperating.model.DealShopperHistory',
     
-    proxy : new XMLifeOperating.generic.BaseProxy('deal/shopperHistory','arrayResult'),
+    proxy : new XMLifeOperating.generic.BaseProxy('deal/shopperHistory','result'),
     sorters: [{
     	property: 'created',
     	direction: 'DESC',
