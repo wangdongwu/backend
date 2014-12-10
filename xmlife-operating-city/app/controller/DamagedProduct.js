d72ca2bbdd528838e2649499a04df0ff83ec2c7f
sunbin <sbin@ipaitao.com>
2014-12-10 17:50:01 +0800

    删除HEAD
    Test Plan: view
    
    Reviewers: ljlong, yls, ryl
    
    Reviewed By: ryl
    
    Subscribers: qc
    
    Differential Revision: http://192.168.6.4/D161

--- xmlife-operating-city/app/controller/DamagedProduct.js
+++ xmlife-operating-city/app/controller/DamagedProduct.js
@@ -18,8 +18,6 @@ Ext.define('XMLifeOperating.controller.DamagedProduct', {
             'damagedGoodsList #shopArea': {
                 click: function() {
                     var store = self.getShopAreaStore();
-
-<<<<<<< HEAD
         }
       },
       'damagedGoodsList #export': {
@@ -62,65 +60,6 @@ Ext.define('XMLifeOperating.controller.DamagedProduct', {
       'damagedGoodsList #acceptBtn': {
         click: function () {
           var id = arguments[5].get("id");
-=======
-                    console.log(XMLifeOperating.generic.Global.currentCity);
-                    store.getProxy().extraParams = {
-                        city: XMLifeOperating.generic.Global.currentCity
-                    }
-                    store.load();
-                }
-            },
-            'damagedGoodsList #search': {
-                click: function(button) {
-                    self.queryParams.areaId = button.up("panel").down("#shopArea").getValue();
-                    self.queryParams.startTime = self.formatDate(button.up("panel").down("datefield[name='startTime']").getValue());
-                    self.queryParams.endTime = self.formatDate(button.up("panel").down("datefield[name='endTime']").getValue());
-                    self.queryParams.status = button.up("panel").down("#status").getValue();
-                    self.queryParams.cityId = XMLifeOperating.generic.Global.currentCity;
-                    self.reloadProductList();
-                }
-            },
-            'damagedGoodsList #export': {
-                click: function(button) {
-                    var areaId = button.up("panel").down("#shopArea").getValue(),
-                        startTime = button.up("panel").down("datefield[name='startTime']").getValue(),
-                        endTime = button.up("panel").down("datefield[name='endTime']").getValue(),
-                        status = button.up("panel").down("#status").getValue();
-                    if (areaId == null) {
-                        areaId = "";
-                    };
-                    if (status == null) {
-                        status = "";
-                    };
-                    var sessionId = localStorage.getItem('sessionId') || '';
-                    var url = XMLifeOperating.generic.Global.URL.biz + 'damagedProductApply/exportAuditList' +
-                        '?cityId=' + XMLifeOperating.generic.Global.currentCity +
-                        '&areaId=' + areaId +
-                        '&startTime=' + self.formatDate(startTime) +
-                        '&endTime=' + self.formatDate(endTime) +
-                        '&status=' + status +
-                        '&sessionId=' + sessionId;
-                    window.open(url, '_blank');
-                }
-            },
-            'damagedGoodsList #batchAccept': {
-                click: function(button) {
-                    var selectedItems = button.up("panel").getSelectionModel().getSelection(),
-                        params = {},
-                        itemIds = [];
-                    for (var i = 0; i < selectedItems.length; i++) {
-                        itemIds.push(selectedItems[i].get('id'));
-                    }
-                    params['ids'] = itemIds;
-                    params['pass'] = true;
-                    self.auditProduct(params);
-                }
-            },
-            'damagedGoodsList #acceptBtn': {
-                click: function() {
-                    var id = arguments[5].get("id");
->>>>>>> 8a2d9a6... 订单中心
-
                     self.auditProduct({
                         'ids': [id],
                         'pass': true
