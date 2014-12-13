2fa5c28ddd60fac34dbe3e0f76f4932011dae8cc
yelinsen <yls@ipaitao.com>
2014-12-13 18:54:27 +0800

    修改 前面对城市残损修改冲突引起得误删代码
    Summary: 修改 前面对城市残损修改冲突引起得误删代码
    
    Test Plan: 1.3.2
    
    Reviewers: wwb, zz, ryl, wdw, sbin
    
    Reviewed By: sbin
    
    Subscribers: qc
    
    Differential Revision: http://192.168.6.4/D218

--- xmlife-operating-city/app/controller/DamagedProduct.js
+++ xmlife-operating-city/app/controller/DamagedProduct.js
@@ -18,54 +18,75 @@ Ext.define('XMLifeOperating.controller.DamagedProduct', {
             'damagedGoodsList #shopArea': {
                 click: function() {
                     var store = self.getShopAreaStore();
-        }
-      },
-      'damagedGoodsList #export': {
-        click: function (button) {
-          var areaId = button.up("panel").down("#shopArea").getValue(),
-              startTime = button.up("panel").down("datefield[name='startTime']").getValue(),
-              endTime = button.up("panel").down("datefield[name='endTime']").getValue(),
-              status = button.up("panel").down("#status").getValue();
-          if (areaId == null) {
-              areaId = "";
-          };
-          if (status == null) {
-              status = "";
-          };
-          var sessionId = localStorage.getItem('sessionId') || '';
-          var url = XMLifeOperating.generic.Global.URL.biz + 'damagedProductApply/exportAuditList' +
-            '?cityId=' + XMLifeOperating.generic.Global.currentCity +
-            '&areaId=' + areaId +
-            '&startTime=' + self.formatDate(startTime) +
-            '&endTime=' + self.formatDate(endTime) +
-            '&status=' + status +
-            '&sessionId=' + sessionId;
+                    console.log(XMLifeOperating.generic.Global.currentCity);
+                    store.getProxy().extraParams = {
+                        city: XMLifeOperating.generic.Global.currentCity
+                    }
+                    store.load();
+                }
+            },
 
-          window.open(url, '_blank');
-        }
-      },
-      'damagedGoodsList #batchAccept': {
-        click: function (button) {
-          var selectedItems = button.up("panel").getSelectionModel().getSelection(),
-            params = {},
-            itemIds = [];
-          for (var i = 0; i < selectedItems.length; i++) {
-            itemIds.push(selectedItems[i].get('id'));
-          }
-          params['ids'] = itemIds;
-          params['pass'] = true;
-          self.auditProduct(params);
-        }
-      },
-      'damagedGoodsList #acceptBtn': {
-        click: function () {
-          var id = arguments[5].get("id");
+            'damagedGoodsList #search': {
+                click: function(button) {
+                    self.queryParams.areaId = button.up("panel").down("#shopArea").getValue();
+                    self.queryParams.startTime = self.formatDate(button.up("panel").down("datefield[name='startTime']").getValue());
+                    self.queryParams.endTime = self.formatDate(button.up("panel").down("datefield[name='endTime']").getValue());
+                    self.queryParams.status = button.up("panel").down("#status").getValue();
+                    self.queryParams.cityId = XMLifeOperating.generic.Global.currentCity;
+
+                    self.reloadProductList();
+                }
+            },
+
+            'damagedGoodsList #export': {
+                click: function(button) {
+                    var areaId = button.up("panel").down("#shopArea").getValue(),
+                        startTime = button.up("panel").down("datefield[name='startTime']").getValue(),
+                        endTime = button.up("panel").down("datefield[name='endTime']").getValue(),
+                        status = button.up("panel").down("#status").getValue();
+                    if (areaId == null) {
+                        areaId = "";
+                    };
+                    if (status == null) {
+                        status = "";
+                    };
+                    var sessionId = localStorage.getItem('sessionId') || '';
+                    var url = XMLifeOperating.generic.Global.URL.biz + 'damagedProductApply/exportAuditList' +
+                        '?cityId=' + XMLifeOperating.generic.Global.currentCity +
+                        '&areaId=' + areaId +
+                        '&startTime=' + self.formatDate(startTime) +
+                        '&endTime=' + self.formatDate(endTime) +
+                        '&status=' + status +
+                        '&sessionId=' + sessionId;
+
+                    window.open(url, '_blank');
+                }
+            },
+
+            'damagedGoodsList #batchAccept': {
+                click: function(button) {
+                    var selectedItems = button.up("panel").getSelectionModel().getSelection(),
+                        params = {},
+                        itemIds = [];
+                    for (var i = 0; i < selectedItems.length; i++) {
+                        itemIds.push(selectedItems[i].get('id'));
+                    }
+                    params['ids'] = itemIds;
+                    params['pass'] = true;
+                    self.auditProduct(params);
+                }
+            },
+
+            'damagedGoodsList #acceptBtn': {
+                click: function() {
+                    var id = arguments[5].get("id");
                     self.auditProduct({
                         'ids': [id],
                         'pass': true
                     });
                 }
             },
+
             'damagedGoodsList #rejectBtn': {
                 click: function() {
                     var id = arguments[5].get("id");
@@ -91,11 +112,13 @@ Ext.define('XMLifeOperating.controller.DamagedProduct', {
             }
         })
     },
+
     reloadProductList: function() {
         var productStore = this.getDamagedProductStore();
         productStore.getProxy().extraParams = this.queryParams;
         productStore.load();
     },
+    
     formatDate: function(date) {
         return date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
     }
