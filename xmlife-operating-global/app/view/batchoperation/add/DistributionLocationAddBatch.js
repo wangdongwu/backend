11139215c42c8eb837007f2d4abf2a137b404bb1
ljlong <ljlong@ipaitao.com>
2014-12-17 17:03:20 +0800

    商品模板批量处理视图缺少filename控件BUG修复
    Summary: 商品模板批量处理视图缺少filename控件BUG修复
    
    Test Plan: view code
    
    Reviewers: wwb, wdw, ryl, yls, sbin
    
    Reviewed By: sbin
    
    Subscribers: qc
    
    Differential Revision: http://192.168.6.4/D348

--- xmlife-operating-global/app/view/batchoperation/add/DistributionLocationAddBatch.js
+++ xmlife-operating-global/app/view/batchoperation/add/DistributionLocationAddBatch.js
@@ -1,83 +1,82 @@
 Ext.define('XMLifeOperating.view.batchoperation.add.DistributionLocationAddBatch', {
-  extend: 'Ext.panel.Panel',
-  id : 'DistributionLocationAddBatch',
-  xtype : 'DistributionLocationAddBatch',
-  alias : 'widget.DistributionLocationAddBatch',
-  title : '配送地址',
-  closeAction: 'hide',
-  resizable: false,
-  forceFit: true,
-  items : [
-    {
-      xtype: 'form',
-      edit: false,
-      border: 0,
-      defaults: {
-        bodyPadding : 5
-      },
-      items: [
-        {
-          xtype : 'fieldcontainer',
-          defaultType : 'textfield',
-          defaults : {
-            labelWidth : 80,
-            width :600
-          },
-          items : [
-            {
-              xtype : 'combo',
-              fieldLabel : '城市选择',
-              store : 'SupportedCity',
-              name : 'cityCode',
-              itemId: 'city',
-              allowBlank : false,
-              blankText: '请选择城市',
-              editable : false,
-              triggerAction : 'all',
-              displayField: 'name',
-              valueField: 'code'
+    extend: 'Ext.panel.Panel',
+    id: 'DistributionLocationAddBatch',
+    xtype: 'DistributionLocationAddBatch',
+    alias: 'widget.DistributionLocationAddBatch',
+    title: '配送地址',
+    closeAction: 'hide',
+    resizable: false,
+    forceFit: true,
+    items: [{
+        xtype: 'form',
+        edit: false,
+        border: 0,
+        defaults: {
+            bodyPadding: 5
+        },
+        items: [{
+            xtype: 'fieldcontainer',
+            defaultType: 'textfield',
+            defaults: {
+                labelWidth: 80,
+                width: 600
             },
-            {
-              xtype: 'radiogroup',
-              fieldLabel: '文件类型',
-              items: [
-                {boxLabel: '<span>Excel文件[2007或以上版本](*.<span style="color:red;">xlsx</span>)</span>', name: 'filetype', inputValue: '1', checked: true},
-                {boxLabel: '<span>Excel文件(*.<span style="color:red;">xls</span>)</span>', name: 'filetype', inputValue: '2'}
-              ]
-            },
-            {
-              xtype: 'filefield',
-              fieldLabel: '文件路径',
-              emptyText: '从svn中选择上传',
-              name: 'file'
-            },
-            {
-              xtype: 'textareafield',
-              grow: true,
-              name: 'comment',
-              fieldLabel: '提交说明',
-              allowBlank: false,
-              emptyText: '提交说明(必填)'
-            },
-            {
-              xtype: 'button',
-              text: '提交',
-              width: 50,
-              style: 'margin-left: 430px; margin-top: 10px; color: blue;',
-              itemId: 'submit'
-            },
-            {
-              xtype: 'textareafield',
-              style: 'margin-left: 85px; margin-top: 30px;',
-              edit: false,
-              grow: true,
-              itemId: 'resultLog',
-              width: 515,
-              cols: 40
-            }
-          ]
-        }
-      ]
-    }
-  ]
+            items: [{
+                xtype: 'combo',
+                fieldLabel: '城市选择',
+                store: 'SupportedCity',
+                name: 'cityCode',
+                itemId: 'city',
+                allowBlank: false,
+                blankText: '请选择城市',
+                editable: false,
+                triggerAction: 'all',
+                displayField: 'name',
+                valueField: 'code'
+            }, {
+                xtype: 'radiogroup',
+                fieldLabel: '文件类型',
+                items: [{
+                    boxLabel: '<span>Excel文件[2007或以上版本](*.<span style="color:red;">xlsx</span>)</span>',
+                    name: 'filetype',
+                    inputValue: '1',
+                    checked: true
+                }, {
+                    boxLabel: '<span>Excel文件(*.<span style="color:red;">xls</span>)</span>',
+                    name: 'filetype',
+                    inputValue: '2'
+                }]
+            }, {
+                xtype: 'filefield',
+                fieldLabel: '文件路径',
+                emptyText: '从svn中选择上传',
+                name: 'file'
+            }, {
+                xtype: 'textfield',
+                name: 'filename',
+                hidden: true
+            }, {
+                xtype: 'textareafield',
+                grow: true,
+                name: 'comment',
+                fieldLabel: '提交说明',
+                allowBlank: false,
+                emptyText: '提交说明(必填)'
+            }, {
+                xtype: 'button',
+                text: '提交',
+                width: 50,
+                style: 'margin-left: 430px; margin-top: 10px; color: blue;',
+                itemId: 'submit'
+            }, {
+                xtype: 'textareafield',
+                style: 'margin-left: 85px; margin-top: 30px;',
+                edit: false,
+                grow: true,
+                itemId: 'resultLog',
+                width: 515,
+                cols: 40
+            }]
+        }]
+    }]
 });
