5aaf4e94442d8b7e9dd27ff40f76a9c6a8f7afac
ljlong <ljlong@ipaitao.com>
2014-12-25 17:31:43 +0800

    城市平台城市下拉列表无proxyBUG修复
    Summary: 城市平台城市下拉列表无proxyBUG修复
    
    Test Plan: view code
    
    Reviewers: qc, wdw, wwb, ryl, yls, sbin
    
    Reviewed By: sbin
    
    Subscribers: qc
    
    Differential Revision: http://192.168.6.4/D677

--- xmlife-operating-city/app/model/SupportedCity.js
+++ xmlife-operating-city/app/model/SupportedCity.js
@@ -1,5 +1,6 @@
 Ext.define('XMLifeOperating.model.SupportedCity', {
     extend: 'Ext.data.Model',
     fields: ['id', 'code', 'name', 'status', 'shopAreas','zones','shipfee','deductd','lng','lat'],
-    autoLoad:false
+    autoLoad:false,
+    proxy:new XMLifeOperating.generic.BaseProxy('supportedcity')
 });
