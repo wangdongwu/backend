7fd438f8d09ba21934333a11cd863eb86b5e3b3d
wangdongwu <wdw@ipaitao.com>
2014-12-26 18:09:13 +0800

    【活动管理】优化autoselect，不要在store美加载完前，去自动选中
    Summary: 【活动管理】优化autoselect，不要在store美加载完前，去自动选中
    
    Test Plan: 【活动管理】优化autoselect，不要在store美加载完前，去自动选中
    
    Reviewers: ljlong, qc, yls, wwb, ryl, yht, sbin, yhtao
    
    Reviewed By: sbin, yhtao
    
    Subscribers: qc
    
    Differential Revision: http://192.168.6.4/D734
    
    Conflicts:
    	xmlife-operating-city/app/controller/PromotionManage.js

--- xmlife-operating-city/app/controller/PromotionManage.js
+++ xmlife-operating-city/app/controller/PromotionManage.js
@@ -247,23 +247,80 @@ Ext.define('XMLifeOperating.controller.PromotionManage', {
             }
           }
         });
-      }
-     });
-	},
-	savePromotionGroup: function (editor, e) {
-		var self = this,
-			model = e.record;
-		sendPostRequest('promotion/group/update', model.getData(), '', '', '', function (response) {
-			if (response.status == 200 && response.responseText) {
-				self.loadPromotionGroupData();
-			}
-		})
-	},
-	renderAddPromotion: function () {
-		if (!this.isPromotionEdit) {
-			var areaIdsCombo = this.getAddPromotion().down('#areaIdsCombo');
-			//this.shopAreaStore.load();
-			areaIdsCombo.setValue(this.shopAreaStore.getRange());
-		}
-	}
+    },
+    /**
+     * [autoSelectPromotion 自动选中以一个活动]
+     * @param  {[type]} combo [当前选择组件]
+     * @return {[type]}       [null]
+     */
+    autoSelectPromotion: function (combo) {
+      var firstPromotion = this.promotionStore.getAt(0);
+        firstPromotion && combo.setValue(firstPromotion.get('id'));
+    },
+    showDetailProduct : function(){
+      var self = this,
+          model = arguments[3],
+          groupId = model.get('groupId');
+          self.sromotionGroupProductStore.load({
+            params : {
+              promotionId : self.currentPromotionId,
+              groupId : groupId
+            }
+          });
+      return false;
+    },
+    /**
+     * [loadPromotionGroupData 加载活动组store]
+     * @param  {[type]} combo         [当前选择组件]
+     * @param  {[type]} promotionId [选中的promotionId]
+     * @return {[type]}             [null]
+     */
+    loadPromotionGroupData: function (combo, promotionId) {
+        this.sromotionGroupProductStore.loadData([]);
+        this.currentPromotionId = promotionId || this.currentPromotionId;
+        this.sromotionGroupStore.load({
+            params: {
+                promotionId: this.currentPromotionId
+            }
+        });
+    },
+    /**
+     * [uploadPromotion 提交上传活动组的excel文件]
+     * @param  {[type]} button [description]
+     * @return {[type]}        [description]
+     */
+    uploadPromotion: function (button) {
+        var self = this,
+            form = button.up('form'),
+            sessionId = localStorage.getItem('sessionId') || '';
+
+        form.submit({
+            url: XMLifeOperating.generic.Global.URL.biz + 'promotion/group/add?sessionId=' + sessionId,
+            success: function (form, action) {
+                var res = action.response.responseText;
+                if (res) {
+                    self.loadPromotionGroupData();
+                }
+            },
+            failure: function (form, action) {
+                var res = action.response.responseText;
+                if (res) {
+                    self.loadPromotionGroupData();
+                }
+            }
+        });
+    },
+    savePromotionGroup: function (editor, e) {
+        var self = this,
+            model = e.record;
+        sendPostRequest('promotion/group/update', model.getData(), '', '', '', function (response) {
+            if (response.status == 200 && response.responseText) {
+                self.loadPromotionGroupData();
+            }
+        })
+    }
+    ,
+    renderAddPromotion: function () {
+        shopAreaStore.load();
+    }
 });
