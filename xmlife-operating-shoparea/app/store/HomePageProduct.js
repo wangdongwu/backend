1629c15b96bcf101e798864310c288f5b9b9361a
wwb <wwb@ipaitao.com>
2015-01-07 11:25:49 +0800

    中心首页/店铺积木配置SKU v1.5
    Summary:
    1、中心首页/店铺积木配置SKU；
    2、店铺首页代码优化整理。
    
    Test Plan: 见效果图
    
    Reviewers: yls, qc, ljlong, yhtao, ryl, sbin, wdw
    
    Reviewed By: wdw
    
    Subscribers: qc
    
    Differential Revision: http://192.168.6.4/D904

--- xmlife-operating-shoparea/app/store/HomePageProduct.js
+++ xmlife-operating-shoparea/app/store/HomePageProduct.js
@@ -1,5 +1,10 @@
 Ext.define('XMLifeOperating.store.HomePageProduct', {
 	extend: 'Ext.data.Store',
 	model: 'XMLifeOperating.model.HomePageProduct',
+	sorters: [{
+        sorterFn: function(o1, o2){
+            return o1.get('skuId') < o2.get('skuId') ? -1 : 1;
+        }
+    }],
 	proxy: new XMLifeOperating.generic.BaseProxy('product/getValidProduct')
 });
