cbfb83fd320943b5cbbe2765f96010a7f79da70e
wangdongwu <wdw@ipaitao.com>
2015-01-12 10:28:08 +0800

    【全局】图片上传改造
    Summary: 【全局】图片上传改造
    
    Test Plan: 【全局】图片上传改造
    
    Reviewers: ljlong, sbin, wwb, yhtao, qc, yls, ryl
    
    Reviewed By: ryl
    
    Subscribers: qc
    
    Differential Revision: http://192.168.6.4/D1591

--- xmlife-operating-global/app/controller/BatchOperation.js
+++ xmlife-operating-global/app/controller/BatchOperation.js
@@ -1,211 +1,277 @@
 Ext.define('XMLifeOperating.controller.BatchOperation', {
-  extend: 'Ext.app.Controller',
-  views: [
-    'batchoperation.BatchAddPanel',
-    'batchoperation.BatchUpdatePanel',
-    'batchoperation.BatchUploadSimple',
-    'batchoperation.BatchUploadWithLocation',
-    'batchoperation.update.ProductCategoryUpdateBatch',
-    'batchoperation.update.ProductCategoryTemplateUpdateBatch',
-    'batchoperation.update.ProductDescriptionUpdateBatch',
-    'batchoperation.update.ProductNameUpdateBatch',
-    'batchoperation.update.ProductPictureUpdateBatch',
-    'batchoperation.update.ProductPriceUpdateBatch',
-    'batchoperation.update.ProductRankUpdateBatch',
-    'batchoperation.update.ProductRank2UpdateBatch',
-    'batchoperation.update.ProductBarCodeUpdateBatch',
-    'batchoperation.update.ProductStatusUpdateBatch',
-    'batchoperation.update.ProductStockUpdateBatch',
-    'batchoperation.update.ProductTagUpdateBatch',
-    'batchoperation.update.DistributionTypeUpdateBatch',
-    'batchoperation.add.DistributionLocationAddBatch',
-    'batchoperation.add.ProductInstanceAddBatch',
-    'batchoperation.add.ProductTemplateAddBatch'
-  ],
-  models: [],
-  stores: ['ShopArea', 'Shop'],
-  refs: [
-    {
-      ref: 'ProductNameUpdateBatch',
-      selector: 'ProductNameUpdateBatch',
-      xtype: 'ProductNameUpdateBatch',
-      autoCreate: true
-    }
-  ],
-  init: function () {
-    var self = this;
-    self.control({
-      'ProductNameUpdateBatch #submit': {
-        click: function (button) {
-          self.updateSimple(button, 'backdoor/update/product/name');
-        }
-      },
-      'ProductCategoryTemplateUpdateBatch #submit': {
-        click: function (button) {
-          self.updateSimple(button, 'backdoor/update/product/updatetemplate');
-        }
-      },
-      'ProductDescriptionUpdateBatch #submit': {
-        click: function (button) {
-          self.updateSimple(button, 'backdoor/update/product/desc');
-        }
-      },
-      'ProductRankUpdateBatch #submit': {
-        click: function (button) {
-          self.updateSimple(button, 'backdoor/update/product/rank');
-        }
-      },
-      'ProductRank2UpdateBatch #submit': {
-          click: function(button) {
-              self.updateSimple(button, 'backdoor/update/product/rank2');
-          }
-      },
-      'ProductBarCodeUpdateBatch #submit': {
-          click: function(button) {
-              self.updateSimple(button, 'backdoor/update/product/barCode');
-          }
-      },
-      'ProductTagUpdateBatch #submit': {
-        click: function (button) {
-          self.updateSimple(button, 'backdoor/update/product/tag');
-        }
-      },
-      'ProductPriceUpdateBatch #areaId': {
-        select: self.loadShopData
-      },
-      'ProductPriceUpdateBatch #submit': {
-        click: function (button) {
-          self.updateSimple(button, 'backdoor/update/product/price');
-        }
-      },
-      'ProductStatusUpdateBatch #areaId': {
-        select: self.loadShopData
-      },
-      'ProductStatusUpdateBatch #submit': {
-        click: function (button) {
-          self.updateSimple(button, 'backdoor/update/product/status');
-        }
-      },
-      'ProductStockUpdateBatch #areaId': {
-        select: self.loadShopData
-      },
-      'ProductStockUpdateBatch #submit': {
-        click: function (button) {
-          self.updateSimple(button, 'backdoor/update/product/stock');
-        }
-      },
-      'ProductCategoryUpdateBatch #areaId': {
-        select: self.loadShopData
-      },
-      'ProductCategoryUpdateBatch #submit': {
-        click: function (button) {
-          self.updateSimple(button, 'product/resetCategory');
-        }
-      },
-      'ProductPictureUpdateBatch #areaId': {
-        select: self.loadShopData
-      },
-      'ProductPictureUpdateBatch #submit': {
-        click: function (button) {
-          self.updateAndResumeMultiFileInput(button, 'backdoor/update/product/img2')
-        }
-      },
-      'DistributionTypeUpdateBatch #submit': {
-        click: function (button) {
-          self.updateSimple(button, 'residentalDistrict/update/district/type');
-        }
-      },
-      'ProductTemplateAddBatch #submit': {
-        click: function (button) {
-          self.updateAndResumeMultiFileInput(button, 'backdoor/update/product/addtemplates2')
-        }
-      },
-      'DistributionLocationAddBatch #submit': {
-        click: function (button) {
-          self.updateSimple(button, 'residentalDistrict/genDistrict');
-        }
-      },
-      'ProductInstanceAddBatch #areaId': {
-        select: self.loadShopData
-      },
-      'ProductInstanceAddBatch #submit': {
-        click: function (button) {
-          self.updateSimple(button, 'product/appendProducts');
-        }
-      }
-    });
-  },
-  updateSimple: function (button, url) {
-    var form = button.up('form').getForm(),
-      path = button.up("fieldcontainer").down("filefield").getValue(),
-      logArea = button.up('form').down('#resultLog'),
-      arr = path.split('\\'),
-      filename = arr[arr.length-1].split('.')[0];
-      button.up('form').down('textfield[name="filename"]').setValue(filename);
+	extend: 'Ext.app.Controller',
+	views: [
+		'batchoperation.BatchAddPanel',
+		'batchoperation.BatchUpdatePanel',
+		'batchoperation.BatchUploadSimple',
+		'batchoperation.BatchUploadWithLocation',
+		'batchoperation.update.ProductCategoryUpdateBatch',
+		'batchoperation.update.ProductCategoryTemplateUpdateBatch',
+		'batchoperation.update.ProductDescriptionUpdateBatch',
+		'batchoperation.update.ProductNameUpdateBatch',
+		'batchoperation.update.ProductPictureUpdateBatch',
+		'batchoperation.update.ProductPriceUpdateBatch',
+		'batchoperation.update.ProductRankUpdateBatch',
+		'batchoperation.update.ProductRank2UpdateBatch',
+		'batchoperation.update.ProductBarCodeUpdateBatch',
+		'batchoperation.update.ProductStatusUpdateBatch',
+		'batchoperation.update.ProductStockUpdateBatch',
+		'batchoperation.update.ProductTagUpdateBatch',
+		'batchoperation.update.DistributionTypeUpdateBatch',
+		'batchoperation.add.DistributionLocationAddBatch',
+		'batchoperation.add.ProductInstanceAddBatch',
+		'batchoperation.add.ProductTemplateAddBatch'
+	],
+	models: [],
+	stores: ['ShopArea', 'Shop'],
+	refs: [{
+		ref: 'ProductNameUpdateBatch',
+		selector: 'ProductNameUpdateBatch',
+		xtype: 'ProductNameUpdateBatch',
+		autoCreate: true
+	}],
+	init: function () {
+		var self = this;
+		self.control({
+			'ProductNameUpdateBatch #submit': {
+				click: function (button) {
+					self.updateSimple(button, 'backdoor/update/product/name');
+				}
+			},
+			'ProductCategoryTemplateUpdateBatch #submit': {
+				click: function (button) {
+					self.updateSimple(button, 'backdoor/update/product/updatetemplate');
+				}
+			},
+			'ProductDescriptionUpdateBatch #submit': {
+				click: function (button) {
+					self.updateSimple(button, 'backdoor/update/product/desc');
+				}
+			},
+			'ProductRankUpdateBatch #submit': {
+				click: function (button) {
+					self.updateSimple(button, 'backdoor/update/product/rank');
+				}
+			},
+			'ProductRank2UpdateBatch #submit': {
+				click: function (button) {
+					self.updateSimple(button, 'backdoor/update/product/rank2');
+				}
+			},
+			'ProductBarCodeUpdateBatch #submit': {
+				click: function (button) {
+					self.updateSimple(button, 'backdoor/update/product/barCode');
+				}
+			},
+			'ProductTagUpdateBatch #submit': {
+				click: function (button) {
+					self.updateSimple(button, 'backdoor/update/product/tag');
+				}
+			},
+			'ProductPriceUpdateBatch #areaId': {
+				select: self.loadShopData
+			},
+			'ProductPriceUpdateBatch #submit': {
+				click: function (button) {
+					self.updateSimple(button, 'backdoor/update/product/price');
+				}
+			},
+			'ProductStatusUpdateBatch #areaId': {
+				select: self.loadShopData
+			},
+			'ProductStatusUpdateBatch #submit': {
+				click: function (button) {
+					self.updateSimple(button, 'backdoor/update/product/status');
+				}
+			},
+			'ProductStockUpdateBatch #areaId': {
+				select: self.loadShopData
+			},
+			'ProductStockUpdateBatch #submit': {
+				click: function (button) {
+					self.updateSimple(button, 'backdoor/update/product/stock');
+				}
+			},
+			'ProductCategoryUpdateBatch #areaId': {
+				select: self.loadShopData
+			},
+			'ProductCategoryUpdateBatch #submit': {
+				click: function (button) {
+					self.updateSimple(button, 'product/resetCategory');
+				}
+			},
+			'ProductPictureUpdateBatch #areaId': {
+				select: self.loadShopData
+			},
+			'ProductPictureUpdateBatch #submit': {
+				click: function (button) {
+					self.updateAndResumeMultiFileInput(button, 'backdoor/update/product/img', true)
+				}
+			},
+			'DistributionTypeUpdateBatch #submit': {
+				click: function (button) {
+					self.updateSimple(button, 'residentalDistrict/update/district/type');
+				}
+			},
+			'ProductTemplateAddBatch #submit': {
+				click: function (button) {
+					self.updateAndResumeMultiFileInput(button, 'backdoor/update/product/addtemplates', true)
+				}
+			},
+			'DistributionLocationAddBatch #submit': {
+				click: function (button) {
+					self.updateSimple(button, 'residentalDistrict/genDistrict');
+				}
+			},
+			'ProductInstanceAddBatch #areaId': {
+				select: self.loadShopData
+			},
+			'ProductInstanceAddBatch #submit': {
+				click: function (button) {
+					self.updateSimple(button, 'product/appendProducts');
+				}
+			}
+		});
+	},
+	updateSimple: function (button, url) {
+		var form = button.up('form').getForm(),
+			path = button.up("fieldcontainer").down("filefield").getValue(),
+			logArea = button.up('form').down('#resultLog'),
+			arr = path.split('\\'),
+			filename = arr[arr.length - 1].split('.')[0];
+		button.up('form').down('textfield[name="filename"]').setValue(filename);
 
-    form.url = XMLifeOperating.generic.Global.URL.biz + url;
-    this.submitForm(form, logArea, path);
-  },
-  updateAndResumeMultiFileInput: function (button, url) {
-    var form = button.up('form').getForm(),
-      path = button.up("fieldcontainer").down("filefield").getValue(),
-      logArea = button.up('form').down('#resultLog');
+		form.url = XMLifeOperating.generic.Global.URL.biz + url;
+		this.submitForm(form, logArea, path);
+	},
+	updateAndResumeMultiFileInput: function (button, url, syncImg) {
+		var self = this,
+			form = button.up('form').getForm(),
+			path = '/',
+			logArea = button.up('form').down('#resultLog'),
+			sessionId = localStorage.getItem('sessionId'),
+			syncImgUrl = 'http://192.168.6.101:12345/proxy/http://192.168.6.101/jenkins/job/SyncSkuResource/build?delay=0sec';
+		if (syncImg) {
+			Ext.Ajax.request({
+				url: syncImgUrl,
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded'
+				},
+				params: {
+					json: Ext.encode({
+						"parameter": [{
+							"name": "session_id",
+							"value": sessionId
+						}, {
+							"name": "dst_env",
+							"value": 'dev'
+						}]
+					})
+				},
+				method: 'POST',
+				success: function (response) {
+					//触发成功
+					if (response.status == 201) {
+						self.getBuildResult(function(){
+              form.url = XMLifeOperating.generic.Global.URL.biz + url;
+              self.submitForm(form, logArea, path, function () {});
+            });
+					}
+				},
+				failure: function (response, opts) {
+					//触发失败
+				}
+			});
+		}
+	},
+	getBuildResult: function (callback) {
+		var self = this,
+			runner = new Ext.util.TaskRunner(),
+			resultJsonUrl = 'http://192.168.6.101:12345/proxy/http://192.168.6.101/jenkins/job/SyncSkuResource/lastBuild/api/json',
+			getJson = function () {
+				Ext.Ajax.request({
+					url: resultJsonUrl,
+					method: 'GET',
+					success: function (response) {
+						var data = Ext.decode(response.responseText);
+						if (response.status === 200) {
+							if (data.result) {
+                if(data.result === 'SUCCESS'){
+								  self.fireEvent('stopGetJsonResult');
+                }
+							}
+						}
+					},
+					failure: function () {}
+				})
+			},
+      pro = Ext.MessageBox.progress('请等待','上传图片中<a target="_blank" href="http://192.168.6.101/jenkins/job/SyncSkuResource/">查看进度详情..</a>');
+      pro.updateProgress(9/10);
+      
+		var task = runner.newTask({
+			run: getJson,
+			interval: 5000
+		});
+		task.start();
+    self.on('stopGetJsonResult',function(){
+      task.stop();
+      pro.hide();
+      Ext.Msg.alert('提示','图片更新成功');
+      callback && callback();
+    })
+	},
+	loadShopData: function (combo, records, opts) {
+		if (records.length == 0) return;
 
-    form.url = XMLifeOperating.generic.Global.URL.biz + url;
-    this.submitForm(form, logArea, path, function () {
-      button.up('form').down('#pictures').fileInputEl.set({multiple: 'multiple'});
-    });
-  },
-  loadShopData: function(combo, records, opts) {
-    if (records.length == 0) return;
-
-    var shopStore = this.getShopStore();
-    var shopCombo = combo.up('form').down('#shopId')
-    shopCombo.setValue('');
-    shopStore.load({
-      params: {areaId: records[0].data.id}
-    });
-  },
-  submitForm: function (form, logArea, path, cb) {
-    var sessionId = localStorage.getItem('sessionId') || '';
-    if (form.isValid()) {
-      if (form.url.indexOf('sessionId') < 0) {
-        form.url = form.url + '?sessionId=' + sessionId;
-      }
-      form.submit({
-        waitMsg: '正在上传您的文件......',
-        success: function (form, action) {
-          if (cb) {
-            cb();
-          }
-          this.customMethod(form, action);
-        },
-        failure: function (form, action) {
-          if (cb) {
-            cb();
-          }
-          this.customMethod(form, action);
-        },
-        customMethod: function (form, action) {
-          var data = JSON.parse(action.response.responseText);
-          var date = new Date(data.logTime);
-          var dataStr = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes();
-          msg = "<p><span style='color: blue;'>日志信息: " + dataStr + "</span><br>";
-          msg += "----------------------------------------<br><br>";
-          msg += '[Msg] 导入开始…… <br>';
-          msg += "[Msg] 导入类型：Excel文件[2007或以上版本]<br>";
-          msg += "[Msg] 导入路径: " + path + "<br>";
-          msg += "[Msg] 执行统计：" + data.runMsg[0] + "<br>";
-          msg += "[Msg] 提交说明：" + data.comment + "<br>";
-          if (data.runResult[0] === 1) {
-            msg += "[Msg] 执行结果：<span style='color: blue;'>成功</span></p>"
-          } else {
-            msg += "[Msg] 错误信息：" + data.errorMsg + "<br>";
-            msg += "[Msg] 执行结果：<span style='color: red;'>失败</span></p>";
-          }
-          logArea.update(msg);
-        }
-      });
-    }
-  }
+		var shopStore = this.getShopStore();
+		var shopCombo = combo.up('form').down('#shopId')
+		shopCombo.setValue('');
+		shopStore.load({
+			params: {
+				areaId: records[0].data.id
+			}
+		});
+	},
+	submitForm: function (form, logArea, path, cb) {
+		var sessionId = localStorage.getItem('sessionId') || '';
+		if (form.isValid()) {
+			if (form.url.indexOf('sessionId') < 0) {
+				form.url = form.url + '?sessionId=' + sessionId;
+			}
+			form.submit({
+				waitMsg: '正在上传您的文件......',
+				success: function (form, action) {
+					if (cb) {
+						cb();
+					}
+					this.customMethod(form, action);
+				},
+				failure: function (form, action) {
+					if (cb) {
+						cb();
+					}
+					this.customMethod(form, action);
+				},
+				customMethod: function (form, action) {
+					var data = Ext.decode(action.response.responseText);
+					var date = new Date(data.logTime);
+					var dataStr = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes();
+					msg = "<p><span style='color: blue;'>日志信息: " + dataStr + "</span><br>";
+					msg += "----------------------------------------<br><br>";
+					msg += '[Msg] 导入开始…… <br>';
+					msg += "[Msg] 导入类型：Excel文件[2007或以上版本]<br>";
+					msg += "[Msg] 导入路径: " + path + "<br>";
+					msg += "[Msg] 执行统计：" + data.runMsg[0] + "<br>";
+					msg += "[Msg] 提交说明：" + data.comment + "<br>";
+					if (data.runResult[0] === 1) {
+						msg += "[Msg] 执行结果：<span style='color: blue;'>成功</span></p>"
+					} else {
+						msg += "[Msg] 错误信息：" + data.errorMsg + "<br>";
+						msg += "[Msg] 执行结果：<span style='color: red;'>失败</span></p>";
+					}
+					logArea.update(msg);
+				}
+			});
+		}
+	}
 });
--- xmlife-operating-global/app/view/batchoperation/update/ProductPictureUpdateBatch.js
+++ xmlife-operating-global/app/view/batchoperation/update/ProductPictureUpdateBatch.js
@@ -1,85 +1,52 @@
 Ext.define('XMLifeOperating.view.batchoperation.update.ProductPictureUpdateBatch', {
-    extend: 'Ext.panel.Panel',
-    id: 'ProductPictureUpdateBatch',
-    xtype: 'ProductPictureUpdateBatch',
-    alias: 'widget.ProductPictureUpdateBatch',
-    title: '商品图片',
-    closeAction: 'hide',
-    resizable: false,
-    forceFit: true,
-    items: [{
-        xtype: 'form',
-        border: 0,
-        edit: false,
-        items: [{
-            xtype: 'fieldcontainer',
-            defaultType: 'textfield',
-            defaults: {
-                labelWidth: 80,
-                width: 600
-            },
-            items: [{
-                    xtype: 'radiogroup',
-                    fieldLabel: '文件类型',
-                    items: [{
-                        boxLabel: '<span>(*.<span style="color:red;">jpg</span>)</span>',
-                        name: 'filetype',
-                        inputValue: '1',
-                        checked: true
-                    }]
-                }, {
-                    xtype: 'textfield',
-                    name: 'filename',
-                    hidden: true
-                }, {
-                    xtype: 'filefield',
-                    fieldLabel: '文件路径',
-                    emptyText: '从svn中选择上传',
-                    itemId: 'pictures',
-                    name: 'file',
-                    listeners: {
-                        afterrender: function(cmp) {
-                            cmp.fileInputEl.set({
-                                multiple: 'multiple'
-                            });
-                        },
-                        change: function(value) {
-                            var files = value.fileInputEl.dom.files,
-                                str = '共' + files.length + '个文件 : ';
-                            showMax = files.length > 3 ? 3 : files.length;
+	extend: 'Ext.panel.Panel',
+	id: 'ProductPictureUpdateBatch',
+	xtype: 'ProductPictureUpdateBatch',
+	alias: 'widget.ProductPictureUpdateBatch',
+	title: '商品图片',
+	closeAction: 'hide',
+	resizable: false,
+	forceFit: true,
+	items: [{
+		xtype: 'form',
+		border: 0,
+		edit: false,
+		items: [{
+			xtype: 'fieldcontainer',
+			defaultType: 'textfield',
+			defaults: {
+				labelWidth: 80,
+				width: 600
+			},
+			items: [{
+					xtype: 'panel',
+					bodyPadding: '5 ,0',
+					html: '<span style="color:red">自动从svn中更新图片</span>'
+				}, {
+					xtype: 'textareafield',
+					grow: true,
+					name: 'comment',
+					fieldLabel: '提交说明',
+					allowBlank: false,
+					emptyText: '提交说明(必填)'
+				}, {
+					xtype: 'button',
+					text: '提交',
+					width: 50,
+					style: 'margin-left: 430px; margin-top: 10px; color: blue;',
+					itemId: 'submit'
+				}, {
+					xtype: 'textareafield',
+					style: 'margin-left: 85px; margin-top: 30px;',
+					edit: false,
+					grow: true,
+					blankText: '执行结果',
+					itemId: 'resultLog',
+					width: 515,
+					cols: 40
+				}
 
-                            for (var i = 0; i < showMax; i++) {
-                                str += files[i].name;
-                                str += '  ';
-                            }
-                            this.inputEl.dom.value = str;
-                        }
-                    }
-                }, {
-                    xtype: 'textareafield',
-                    grow: true,
-                    name: 'comment',
-                    fieldLabel: '提交说明',
-                    allowBlank: false,
-                    emptyText: '提交说明(必填)'
-                }, {
-                    xtype: 'button',
-                    text: '提交',
-                    width: 50,
-                    style: 'margin-left: 430px; margin-top: 10px; color: blue;',
-                    itemId: 'submit'
-                }, {
-                    xtype: 'textareafield',
-                    style: 'margin-left: 85px; margin-top: 30px;',
-                    edit: false,
-                    grow: true,
-                    blankText: '执行结果',
-                    itemId: 'resultLog',
-                    width: 515,
-                    cols: 40
-                }
-
-            ]
-        }]
-    }]
+			]
+		}]
+	}]
 });
