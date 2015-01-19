72937b5f3ac345503e3539446073d709e1068ef3
ryl <ryl@ipaitao.com>
2015-01-19 11:44:26 +0800

    Description： 解决编译不过的bug author: ryl Bug ID: Release：1.5
    Summary: 	解决编译不过的bug author: ryl Bug ID: Release：1.5
    
    Test Plan: 	have a look
    
    Reviewers: yls, wwb, qc, wdw, ljlong, zz, sbin, yhtao
    
    Reviewed By: sbin, yhtao
    
    Subscribers: qc
    
    Differential Revision: http://192.168.6.4/D1384

--- xmlife-operating-global/app/store/DelivererWorkTime.js
+++ xmlife-operating-global/app/store/DelivererWorkTime.js
@@ -1,11 +1,5 @@
 Ext.define('XMLifeOperating.store.DelivererWorkTime', {
     extend: 'Ext.data.Store',
-<<<<<<< HEAD
-    model: 'XMLifeOperating.model.DelivererWorkTime',
-    proxy: new XMLifeOperating.generic.BaseProxy('deliverer/worktime', 'arrayResult')
-=======
     model:'XMLifeOperating.model.DelivererWorkTime',
-    
     proxy : new XMLifeOperating.generic.BaseProxy('deliverer/worktime','result')
->>>>>>> 退货申请页面接口调试
 });
