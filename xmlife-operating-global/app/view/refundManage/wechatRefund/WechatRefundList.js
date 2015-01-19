7a1db9e11a1c93384279bdd1dd771a7a9303d27a
ryl <ryl@ipaitao.com>
2015-01-19 12:06:08 +0800

    Description： 【全局退款后台】界面交互改进 对于退款金额>100的条目，整体采用红色字体。bug author: ryl Bug ID: MJS-1929 Release：1.5
    Summary: 	【全局退款后台】界面交互改进 对于退款金额>100的条目，整体采用红色字体。bug author: ryl Bug ID: MJS-1929 Release：1.5
    
    Test Plan: 	have a look
    
    Reviewers: yls, wwb, qc, wdw, ljlong, zz, yhtao, sbin
    
    Reviewed By: sbin
    
    Subscribers: qc
    
    Differential Revision: http://192.168.6.4/D1388

--- xmlife-operating-global/app/view/refundManage/wechatRefund/WechatRefundList.js
+++ xmlife-operating-global/app/view/refundManage/wechatRefund/WechatRefundList.js
@@ -277,7 +277,7 @@ Ext.define('XMLifeOperating.view.refundManage.wechatRefund.WechatRefundList', {
         align: 'left',
         renderer: function(value) {
             var str = value / 100;
-            return str >= 100 ? '<span style="color:red;">'+ str +'</span>' : str;
+            return str > 100 ? '<span style="color:red;">'+ str +'</span>' : str;
         }
     }, {
         text: '处理状态',
