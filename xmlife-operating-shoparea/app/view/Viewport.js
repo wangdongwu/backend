e2d92a1e53694da8454f8c81315f45dd57df70f7
wangdongwu <wdw@ipaitao.com>
2015-01-30 17:31:20 +0800

    添加证书共外网用户下载使用
    Summary: 添加证书共外网用户下载使用
    
    Test Plan: 添加证书共外网用户下载使用
    
    Reviewers: ljlong, wwb, sbin, yhtao, qc, yls, ryl
    
    Reviewed By: ryl
    
    Differential Revision: http://192.168.6.4/D1953

--- xmlife-operating-shoparea/app/view/Viewport.js
+++ xmlife-operating-shoparea/app/view/Viewport.js
@@ -29,7 +29,7 @@ Ext.define('XMLifeOperating.view.Viewport', {
             {
                 closable : false,
                 title : '欢迎使用',
-                html: '<br/><br/><br/><br/><h1 style="text-align:center">欢迎使用小美后台管理系统!! <br/><br/>⊙o⊙</h1><p style="font-size:14px;text-align:center">由于小美后台升级https，为大家更安全更正常的使用请<a target="_blank" href="http://www.xiaomei.com/download/%E5%B0%8F%E7%BE%8Ehttps%E8%AF%81%E4%B9%A6%E5%AE%89%E8%A3%85%E8%AF%B4%E6%98%8E.zip">小美https证书安装说明.zip</a></p>'
+                html: '<br/><br/><br/><br/><h1 style="text-align:center">欢迎使用小美后台管理系统!! <br/><br/>⊙o⊙</h1><p style="font-size:14px;text-align:center">由于小美后台升级https，为大家更安全更正常的使用请<a target="_blank" href="http://www.xiaomei.com/downloads/%E5%B0%8F%E7%BE%8Ehttps%E8%AF%81%E4%B9%A6%E5%AE%89%E8%A3%85%E8%AF%B4%E6%98%8E.zip">小美https证书安装说明.zip</a></p>'
             }
             ],
             layout: 'fit',
