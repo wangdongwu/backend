263f4a57ef4957dbe2e493158494da3d5b88b477
wwb <wwb@ipaitao.com>
2015-02-06 00:52:51 +0800

    全局电话正则增加17*手机
    Summary: 全局电话正则增加17*手机
    
    Test Plan: 见截图
    
    Reviewers: yls, qc, yhtao, ryl, sbin, ljlong
    
    Reviewed By: ljlong
    
    Subscribers: qc
    
    Differential Revision: http://192.168.6.4/D2034

--- xmlife-operating-global/app/generic/Global.js
+++ xmlife-operating-global/app/generic/Global.js
@@ -1,10 +1,10 @@
-Ext.define("XMLifeOperating.generic.Global",{
+Ext.define("XMLifeOperating.generic.Global", {
     statics: {
         URL: window.URLMap,
-        SERVICECENEERID:2,
+        SERVICECENEERID: 2,
         currentCity: 330100,
         VALIDATION_CONSTANTS: {
-            PHONE: /^(86){0,1}(13[0-9]|15[012356789]|18[0-9]|14[57])[0-9]{8}$/,
+            PHONE: /^(86){0,1}(13[0-9]|15[0-9]|17[0-9]|18[0-9]|14[57])[0-9]{8}$/,
             ID_NO: /^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])(\d{3})(\d|X)$/,
             PRICE: /^([0-9]{1}\d*)(\.(\d){1,2})?$/,
             URL: /^http:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?$/,
@@ -16,10 +16,10 @@ Ext.define("XMLifeOperating.generic.Global",{
         operating_type: 'global',
         // operating_type: 'center',
         current_operating: -1,
-        getLength : function(str){
-          //计算字符串的长途 
-           var c = str.match(/[^ -~]/g);  
-           return str.length + (c ? c.length : 0);  
-       }
+        getLength: function(str) {
+            //计算字符串的长途 
+            var c = str.match(/[^ -~]/g);
+            return str.length + (c ? c.length : 0);
+        }
     }
 });
