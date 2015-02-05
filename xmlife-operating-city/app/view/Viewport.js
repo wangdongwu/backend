d369bcdb8fed805c70b71b1cb266a40d88e7cfdd
sunbin <sbin@ipaitao.com>
2015-02-05 19:09:38 +0800

    小美https证书地址安装说明的请求地址修改
    Test Plan: version:1.5
    
    Reviewers: ljlong, qc, yls, ryl, wwb, yhtao
    
    Reviewed By: yhtao
    
    Subscribers: qc
    
    Differential Revision: http://192.168.6.4/D2022

--- xmlife-operating-city/app/view/Viewport.js
+++ xmlife-operating-city/app/view/Viewport.js
@@ -15,7 +15,6 @@ Ext.define('XMLifeOperating.view.Viewport', {
         width: 200,
         frame: true,
         title: '导航',
-        //split: true,
         collapsible: true
     }, {
         region: 'center',
@@ -26,10 +25,9 @@ Ext.define('XMLifeOperating.view.Viewport', {
         items: [{
             closable: true,
             title: '欢迎使用',
-            html: '<br/><br/><br/><br/><h1 style="text-align:center">欢迎使用小美后台管理系统!! <br/><br/>⊙o⊙</h1>'
+            html: '<br/><br/><br/><br/><h1 style="text-align:center">欢迎使用小美后台管理系统!! <br/><br/>⊙o⊙</h1><p style="font-size:14px;text-align:center">由于小美后台升级https，为大家更安全更正常的使用请<a target="_blank" href="http://cdn.xiaomei.com/data/xiaomei_cert.zip">小美https证书安装说明.zip</a></p>'
         }],
         layout: 'fit',
         header: false
     }]
-
 });
